891565367ec7b9c5c7586dc36b05a9b5b1af5476
danielpq18 <danielpw33@gmail.com>
2026-03-07 13:21:25 -0600

    Modelos

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'slug',
+        'stock',
+        'is_active'
+    ];
+    //podemos hacer relaciones con otras tables, por ejemplo categorias,
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
 }
+
